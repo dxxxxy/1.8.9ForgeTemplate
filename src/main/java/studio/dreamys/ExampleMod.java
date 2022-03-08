3dd9612f5e181ffa344159e85e6c805385269812
DxxxxY <introsdream@gmail.com>
2022-03-07 20:22:53 -0500

    remove pointless files

--- src/main/java/studio/dreamys/ExampleMod.java
+++ src/main/java/studio/dreamys/ExampleMod.java
@@ -2,7 +2,6 @@ package studio.dreamys;
 
 import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 
 @Mod(modid = ExampleMod.MODID, version = ExampleMod.VERSION)
@@ -10,7 +9,7 @@ public class ExampleMod {
     public static final String MODID = "examplemod";
     public static final String VERSION = "1.0";
     
-    @EventHandler
+    @Mod.EventHandler
     public void init(FMLInitializationEvent event) {
 		// some example code
         System.out.println("DIRT BLOCK >> " + Blocks.dirt.getUnlocalizedName());
